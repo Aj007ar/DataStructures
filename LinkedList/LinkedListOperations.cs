d1b912954ef972e26fc67d3b661d24803f244c57
Aj007ar <aj007.ar@gmail.com>
2022-08-27 14:19:25 +0530

    [Ajay] Added: Linkded List operations.

---
+++ LinkedList/LinkedListOperations.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedListOperations
+    {
+        private Node Head;
+        private Node Tail;
+        int Size;
+
+        public int Length()
+        {
+            return Size;
+        }
+
+        public int Last()
+        {
+            return Tail.Value;
+        }
+
+        public void Display()
+        {
+            var temp = Head;
+
+            while (temp != null)
+            {
+                Console.Write(" {0} -> ", temp.Value);
+                temp = temp.Next;
+            }
+            Console.WriteLine("LAST NODE");
+        }
+        public void InsertAtFirst(int item)
+        {
+            var newNode = new Node(item);
+            if (Head == null)
+                Head = Tail = newNode;
+            else
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+            Size += 1;
+        }
+        public void InsertAtLast(int item)
+        {
+            var newNode = new Node(item);
+            if (Head == null)
+                Head = Tail = newNode;
+            else
+            {
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+            Size += 1;
+        }
+        public void InsertAtIndex(int item, int index)
+        {
+            if (index < 0 || index > Size)
+                Console.WriteLine("Index provided is out of range.");
+
+            else if (index == 0)
+                InsertAtFirst(item);
+
+            else if (index == Size)
+                InsertAtLast(item);
+
+            else
+            {
+                var temp = Head;
+                for (int i = 1; i < index; i++)
+                {
+                    temp = temp.Next;
+                }
+
+                var newNode = new Node(item);
+                newNode.Next = temp.Next;
+                temp.Next = newNode;
+                Size++;
+            }
+        }
+        public void GetItemAt(int index)
+        {
+            var temp = Find(index + 1);
+            Console.WriteLine(temp.Value);
+        }
+
+        public int GetIndexOf(int value)
+        {
+            var count = 0;
+            var temp = Head;
+            while (temp != null)
+            {
+                if (temp.Value == value)
+                {
+                    return count;
+                }
+                temp = temp.Next;
+                count++;
+            }
+            return count;
+        }
+        public void AddAfter(int valueAdd, int afterValue)
+        {
+            var index = GetIndexOf(afterValue);
+            InsertAtIndex(valueAdd, index + 1);
+        }
+
+        private Node Find(int index)
+        {
+            if (index < 1 || index >= Size)
+                throw new ArgumentOutOfRangeException();
+            var temp = Head;
+            for (int i = 1; i < index; i++)
+                temp = temp.Next;
+            return temp;
+        }
+        public void DeleteFirst()
+        {
+            if (Head == Tail)
+                Head = Tail = null;
+            else
+            {
+                var temp = Head.Next;
+                Head = temp;
+                Size--;
+            }
+        }
+
+        public void DeleteLast()
+        {
+            if (Head == Tail)
+                Head = Tail = null;
+            else
+            {
+                var temp = Find(Size - 1);
+                Tail = temp;
+                Tail.Next = null;
+                Size--;
+            }
+        }
+
+        public void DeleteAtIndex(int index)
+        {
+            if (index == 0)
+                DeleteFirst();
+            else if (index == Size - 1)
+                DeleteLast();
+            else
+            {
+                var temp = Find(index);
+                temp.Next = temp.Next.Next;
+            }
+        }
+
+        public void DeleteValue(int value)
+        {
+            var index = GetIndexOf(value);
+            DeleteAtIndex(index);
+        }
+
+        public void SortList()
+        {
+            var previous = Head;
+            var current = Head.Next;
+
+            while (current != null)
+            {
+                if (current.Value < previous.Value)
+                {
+                    previous.Next = current.Next;
+                    current.Next = Head;
+                    Head = current;
+
+                    current = previous;
+                }
+                else
+                {
+                    previous = current;
+                    current = current.Next;
+                }
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------> Linked List Operations <-----------------");
+            Console.WriteLine("\n--------------> Create Linked List <-----------------");
+            LinkedListOperations add = new LinkedListOperations();
+            add.InsertAtFirst(70);
+            add.InsertAtFirst(30);
+            add.InsertAtFirst(56);
+            add.Display();
+            Console.WriteLine("\n--------------> Add Value in Linked List <-----------------");
+            LinkedListOperations add2 = new LinkedListOperations();
+            add2.InsertAtLast(56);
+            add2.InsertAtLast(30);
+            add2.InsertAtLast(70);
+            add2.Display();
+            Console.WriteLine("\n--------------> Append Value IN linked list <-----------------");
+            LinkedListOperations Linked = new LinkedListOperations();
+            Linked.InsertAtFirst(56);
+            Linked.InsertAtLast(70);
+            Linked.InsertAtIndex(30, 1);
+            Linked.Display();
+
+            Console.WriteLine("\n--------------> Delete from last  <-----------------");
+            Linked.DeleteLast();
+            Linked.Display();
+            Console.WriteLine("\n--------------> Delete value from first <-----------------");
+            Linked.DeleteFirst();
+            Linked.Display();
+
+            Console.WriteLine("\n--------------> insert at first <-----------------");
+            Linked.InsertAtFirst(56);
+            Console.WriteLine("Index of {0} ", Linked.GetIndexOf(30));
+
+            Console.WriteLine("\n--------------> Insert at last <-----------------");
+            Linked.AddAfter(40, 30);
+            Linked.Display();
+
+            Console.WriteLine("\n--------------> Delete specific value <-----------------");
+            Linked.DeleteValue(40);
+            Linked.Display();
+
+            Console.WriteLine("\n--------------> Calculte length of linked list <-----------------");
+            Console.WriteLine("Length : {0}", Linked.Length());
+
+
+            Console.WriteLine("\n--------------> Sort  the linked list <-----------------");
+            Linked.InsertAtLast(20);
+            Linked.InsertAtLast(10);
+            Linked.InsertAtLast(95);
+            Linked.Display();
+            Linked.SortList();
+            Linked.Display();
+        }
+    }
+}
