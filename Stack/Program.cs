4ed8693f0d4aaa1c9cb7e9f8d506eaf2d1649a15
Aj007ar <aj007.ar@gmail.com>
2022-08-28 16:24:14 +0530

    [Ajay] Added: Stack oprations files.

---
+++ Stack/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Stacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome  to Stack operations");
+            StackOperation stackOperation = new StackOperation(5);
+            Console.WriteLine("\n--------------> Push Elements <--------------------");
+            stackOperation.push(70);
+            stackOperation.push(30);
+            stackOperation.push(56);
+            Console.WriteLine("\n--------------> Pop Elements <--------------------");
+            stackOperation.pop();
+            Console.WriteLine("\n--------------> Peek Elements <--------------------");
+            stackOperation.peek();
+            Console.WriteLine("\n--------------> View All Elements <--------------------");
+            stackOperation.Display();
+        }
+    }
+}
---
+++ Stack/StackOperation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    internal class StackOperation
+    {
+        private int top;
+        private int[] element;
+        private int max;
+        public StackOperation(int size)
+        {
+            element = new int[size];
+            top = -1;
+            max = size;
+        }
+        public void push(int value)
+        {   
+
+            if (top == max - 1)
+            {
+                Console.WriteLine("Stack is overflow");
+                return;
+            }
+            else
+            {
+                element[++top] = value;   
+                Console.WriteLine("{0} Element pushed in stack", element[top]);
+                
+                
+            }
+        }
+        public int pop()
+        {
+            
+                if (top == -1)
+                {
+                    Console.WriteLine("Stack is empty");
+                    return -1;
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Popped from Stack",  element[top]);
+                    return element[top--];
+                }
+
+        }
+        public int peek()
+        {
+
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("{0} is top element from Stack", element[top]);
+                return element[top];
+            }
+
+        }
+        public void Display()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                for(int i = 0; i <= top; i++)
+                {
+                    Console.WriteLine("{0} Element present in stack", element[i]);
+                }
+            }
+        }
+    }
+}
