4e308fba5780b157ab1ac1b92c41e350d4999a91
Aj007ar <aj007.ar@gmail.com>
2022-08-29 00:15:34 +0530

    [Ajay] Added: Queue files

---
+++ Queue/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace QueueOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Queue Operations");
+            QueueOperations queue = new QueueOperations();
+            Console.WriteLine("\n---------------------> Enqueue Operations <-------------------------");
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            Console.WriteLine("After Enqueue");
+            queue.Display();
+            Console.WriteLine("\n---------------------> Dequeue Operations <-------------------------");
+            Console.WriteLine("Dequeue");
+            queue.Dequeue();
+            Console.WriteLine("After Dequeue");
+            queue.Display();
+        }
+    }
+}
---
+++ Queue/QueueOperations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueOP
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class QueueOperations
+    {
+        Node front = null, rear = null;
+        public void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if (this.front == null)
+            {
+                newnode.next = null;
+                this.front = newnode;
+                this.rear = newnode;
+            }
+            else
+            {
+                this.rear.next = newnode;
+                this.rear = newnode;
+            }
+            Console.WriteLine("Enqueued {0}", data);
+        }
+        public void Dequeue()
+        {
+            if (this.front == null)
+            {
+                Console.WriteLine("Empty queue");
+                return;
+            }
+            Console.WriteLine("Dequeued {0}", this.front.data);
+            this.front = this.front.next;
+        }
+        public void Display()
+        {
+            Node temp = this.front;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine(" ");
+        }
+
+    }
+}
